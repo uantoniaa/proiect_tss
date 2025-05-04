57d3d588411a36cc12d6f33310ad9c2109509c15
andreeadv <91892810+andreeadv@users.noreply.github.com>
2025-05-04 21:42:15 +0300

    Updated TagService

--- TagService.php
+++ TagService.php
@@ -1,7 +1,18 @@
 <?php
 
 class TagService {
-    private array $tags = []; // user_id => [note_title => [tag1, tag2, ...]]
+    private array $tags = [];
+    private string $file = "tags.json";
+
+    public function __construct() {
+        if (file_exists($this->file)) {
+            $this->tags = json_decode(file_get_contents($this->file), true) ?? [];
+        }
+    }
+
+    private function save(): void {
+        file_put_contents($this->file, json_encode($this->tags, JSON_PRETTY_PRINT));
+    }
 
     public function addTag(string $userId, string $noteTitle, string $tag): string {
         if (!isset($this->tags[$userId][$noteTitle])) {
@@ -13,6 +24,7 @@ class TagService {
         }
 
         $this->tags[$userId][$noteTitle][] = $tag;
+        $this->save(); // salveaza dupa adaugare
         return "Etichetă adăugată cu succes.";
     }
 
@@ -28,6 +40,7 @@ class TagService {
 
         unset($this->tags[$userId][$noteTitle][$index]);
         $this->tags[$userId][$noteTitle] = array_values($this->tags[$userId][$noteTitle]);
+        $this->save(); // salveaza dupa stergere
         return "Etichetă eliminată.";
     }
 
@@ -45,5 +58,9 @@ class TagService {
         }
 
         return $result;
+    }
+
+    public function getTags(string $userId, string $noteTitle): array {
+        return $this->tags[$userId][$noteTitle] ?? [];
     }
 }
